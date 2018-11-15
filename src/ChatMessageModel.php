8d0832e4bf04a95590a25af4e036fe9da486f6fe
John Geliberte <geliberteofficials@gmail.com>
2018-11-15 16:18:36 +0800

    Fixed multiple entries in inbox

--- src/ChatMessageModel.php
+++ src/ChatMessageModel.php
@@ -359,17 +359,21 @@ class ChatMessageModel {
         $all_messages = [];
         $ctr = 0;
 
+        $temp_senders = [];
         if ($sms_result_from_period->num_rows > 0) {
             while ($row = $sms_result_from_period->fetch_assoc()) {
-                $normalized_number = substr($row["sim_num"], -10);
-                $all_messages[$ctr]['sms_id'] = $row['inbox_id'];
-                $all_messages[$ctr]['full_name'] = strtoupper($row['full_name']);
-                $all_messages[$ctr]['user_number'] = $normalized_number;
-                $all_messages[$ctr]['mobile_id'] = $row['mobile_id'];
-                $all_messages[$ctr]['msg'] = $row['sms_msg'];
-                $all_messages[$ctr]['ts_received'] = $row['ts_sms'];
-                $all_messages[$ctr]['network'] = $this->identifyMobileNetwork($row['sim_num']);
-                $ctr++;
+                if (in_array($row['mobile_id'], $temp_senders, TRUE) != true) {
+                    $normalized_number = substr($row["sim_num"], -10);
+                    $all_messages[$ctr]['sms_id'] = $row['inbox_id'];
+                    $all_messages[$ctr]['full_name'] = strtoupper($row['full_name']);
+                    $all_messages[$ctr]['user_number'] = $normalized_number;
+                    $all_messages[$ctr]['mobile_id'] = $row['mobile_id'];
+                    $all_messages[$ctr]['msg'] = $row['sms_msg'];
+                    $all_messages[$ctr]['ts_received'] = $row['ts_sms'];
+                    $all_messages[$ctr]['network'] = $this->identifyMobileNetwork($row['sim_num']);  
+                    array_push($temp_senders, $row['mobile_id']);
+                    $ctr++;
+                } 
             }
 
             $full_data['data'] = $all_messages;
@@ -377,7 +381,6 @@ class ChatMessageModel {
             echo "0 results\n";
             $full_data['data'] = null;
         }
-
         return $this->utf8_encode_recursive($full_data);
     }
 
@@ -404,20 +407,23 @@ class ChatMessageModel {
         $full_data['type'] = 'smsloadunregisteredinbox';
         $all_messages = [];
         $ctr = 0;
+        $temp_senders = [];
         if ($sms_result_from_period->num_rows > 0) {
             while ($row = $sms_result_from_period->fetch_assoc()) {
-                $normalized_number = substr($row["sim_num"], -10);
-                $all_messages[$ctr]['sms_id'] = $row['inbox_id'];
-                $all_messages[$ctr]['full_name'] = strtoupper($row['full_name']);
-                $all_messages[$ctr]['user_number'] = $normalized_number;
-                $all_messages[$ctr]['mobile_id'] = $row['mobile_id'];
-                $all_messages[$ctr]['user_id'] = $row['user_id'];
-                $all_messages[$ctr]['msg'] = $row['sms_msg'];
-                $all_messages[$ctr]['ts_received'] = $row['ts_sms'];
-                $all_messages[$ctr]['network'] = $this->identifyMobileNetwork($row['sim_num']);
-                $ctr++;
+                if (in_array($row['mobile_id'], $temp_senders, TRUE) != true) {
+                    $normalized_number = substr($row["sim_num"], -10);
+                    $all_messages[$ctr]['sms_id'] = $row['inbox_id'];
+                    $all_messages[$ctr]['full_name'] = strtoupper($row['full_name']);
+                    $all_messages[$ctr]['user_number'] = $normalized_number;
+                    $all_messages[$ctr]['mobile_id'] = $row['mobile_id'];
+                    $all_messages[$ctr]['user_id'] = $row['user_id'];
+                    $all_messages[$ctr]['msg'] = $row['sms_msg'];
+                    $all_messages[$ctr]['ts_received'] = $row['ts_sms'];
+                    $all_messages[$ctr]['network'] = $this->identifyMobileNetwork($row['sim_num']);
+                    $ctr++;
+                    array_push($temp_senders, $row['mobile_id']);
+                } 
             }
-            // array_reverse($all_messages);    
             $full_data['data'] = $all_messages;
         } else {
             echo "0 results\n";
@@ -2513,40 +2519,6 @@ class ChatMessageModel {
                     }
                 }
             }
-
-            // if (sizeof($data->landline) == 0) {
-            //     try {
-            //         $landline_exist = "DELETE FROM user_landlines WHERE user_id='".$data->id."'";
-            //         $result = $this->dbconn->query($landline_exist);
-            //     } catch (Exception $e) {
-            //         $flag = false;
-            //     }
-            // } else {
-            //     for ($landline_counter = 0; $landline_counter < sizeof($data->landline); $landline_counter++) {
-            //         if ($data->landline[$landline_counter]->landline_id != "" && $data->landline[$landline_counter]->landline_number != "") {
-            //             try {
-            //                 $landline_exist = "UPDATE user_landlines SET landline_num = '".$data->landline[$landline_counter]->landline_number."', remarks = '".$data->landline[$landline_counter]->landline_remarks."' WHERE landline_id='".$data->landline[$landline_counter]->landline_id."'";
-            //                 $result = $this->dbconn->query($landline_exist);
-            //             } catch (Exception $e) {
-            //                 $flag = false;
-            //             }
-            //         } else if ($data->landline[$landline_counter]->landline_number == "") {
-            //             try {
-            //                 $landline_exist = "DELETE FROM user_landlines WHERE landline_id='".$data->landline[$landline_counter]->landline_id."'";
-            //                 $result = $this->dbconn->query($landline_exist);
-            //             } catch (Exception $e) {
-            //                 $flag = false;
-            //             }
-            //         } else {
-            //             try {
-            //                 $new_landline = "INSERT INTO user_landlines VALUES (0,'$data->id','".$data->landline[$landline_counter]->landline_number."','".$data->landline[$landline_counter]->landline_remarks."')";
-            //                 $result = $this->dbconn->query($new_landline); 
-            //             } catch (Exception $e) {
-            //                 $flag = false;
-            //             }
-            //         }
-            //     }
-            // }
 
             if ($flag == false) {
                 $return_data['return_msg'] = "Error occured, please refresh the page and try again.";
